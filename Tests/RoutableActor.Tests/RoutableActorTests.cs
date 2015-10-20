e71919bae10028c8a8bddaef9555474229e84722
Jonas Røineslien <jonasro@gmail.com>
2015-10-20 12:00:33 +0200

    Added test for ControllerFactory

--- Tests/RoutableActor.Tests/RoutableActorTests.cs
+++ Tests/RoutableActor.Tests/RoutableActorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace RoutableActor.Tests
@@ -22,6 +23,22 @@ namespace RoutableActor.Tests
             await Task.Delay(100000);
         }
 
+        [Test]
+        public void GetControllerFromfactory()
+        {
+            var controller = ControllerFactory.GetController(2);
+
+            controller.Should().NotBeNull();
+            ControllerFactory.Controllers.Count.Should().Be(1);
+
+            ControllerFactory.GetController(1);
+            ControllerFactory.GetController(6);
+            ControllerFactory.GetController(4);
+
+            ControllerFactory.Controllers.Count.Should().Be(4);
+        }
+
+        
         private void Repeat(int numberOfRepeats, Action action)
         {
             var actions = Enumerable.Repeat(action, numberOfRepeats).ToArray();
