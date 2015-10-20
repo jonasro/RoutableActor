e0cd20ce1a37f1fa0a8970e5124557d6ed2619cd
Jonas Røineslien <jonasro@gmail.com>
2015-10-20 12:00:15 +0200

    Removed controller lookup from FetchHandler

--- RoutableActor/FetchHandler.cs
+++ RoutableActor/FetchHandler.cs
@@ -1,32 +1,11 @@
-﻿using System.Collections.Concurrent;
-
-namespace RoutableActor
+﻿namespace RoutableActor
 {
     public static class FetchHandler
     {
-        private static ConcurrentDictionary<int, Controller> _controllers;
-
-        static FetchHandler()
-        {
-            _controllers = _controllers ?? new ConcurrentDictionary<int, Controller>();
-        }
-
         public static void Handle(Fetch fetch)
         {
-            var controller = GetController(fetch);
+            var controller = ControllerFactory.GetController(fetch.Id);
             controller.Handle(fetch);
-        }
-
-        private static Controller GetController(Fetch fetch)
-        {
-            Controller controller;
-            if (!_controllers.TryGetValue(fetch.Id, out controller))
-            {
-                controller = new Controller(fetch.Id);
-                _controllers.TryAdd(fetch.Id, controller);
-            }
-
-            return controller;
         }
     }
 }
